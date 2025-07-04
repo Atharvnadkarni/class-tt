86b87035bb365d917fe113e59903e24480026434
Atharv Nadkarni <atharvmnadkarni@gmail.com>
2025-07-04 18:02:10 +0530

    remove useless class field

--- backend/models/Teacher.js
+++ backend/models/Teacher.js
@@ -3,14 +3,13 @@ const mongoose = require("mongoose");
 const teacherSchema = new mongoose.Schema({
   _id: false,
   name: String,
-  class: String,
   subjects: [
-      {
-          _id: false,
-          subject: String,
-          classes: { type: mongoose.SchemaTypes.Mixed },
-        },
-    ],
+    {
+      _id: false,
+      subject: String,
+      classes: { type: mongoose.SchemaTypes.Mixed },
+    },
+  ],
   username: { type: String, unique: true, required: true },
   password: { type: String, required: true },
 });
