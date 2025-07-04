72db0cae827d3eada2b74039c5f0c12a40eb777a
Atharv Nadkarni <atharvmnadkarni@gmail.com>
2025-07-04 17:56:27 +0530

    add username field

--- backend/models/Teacher.js
+++ backend/models/Teacher.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const teacherSchema = new mongoose.Schema({
     name: String,
     class: String,
+    username: {type: String, unique: true},
     subjects: [{
         _id: false,
         subject: String,
