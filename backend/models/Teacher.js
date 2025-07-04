94d50459ff1737f1159b1ecc18d13e9149fe15f6
Atharv Nadkarni <atharvmnadkarni@gmail.com>
2025-07-04 17:58:33 +0530

    set unique username

--- backend/models/Teacher.js
+++ backend/models/Teacher.js
@@ -1,17 +1,18 @@
 const mongoose = require("mongoose");
 
 const teacherSchema = new mongoose.Schema({
-    _id: false,
-    name: String,
-    class: String,
-    username: {type: String, unique: true},
-    subjects: [{
-        _id: false,
-        subject: String,
-        classes: {type: mongoose.SchemaTypes.Mixed}
-    }],
-    password: {type: String, required: true}
-})
-
+  _id: false,
+  name: String,
+  class: String,
+  username: { type: String, unique: true, required: true },
+  subjects: [
+    {
+      _id: false,
+      subject: String,
+      classes: { type: mongoose.SchemaTypes.Mixed },
+    },
+  ],
+  password: { type: String, required: true },
+});
 
 module.exports = mongoose.model("Teacher", teacherSchema);
