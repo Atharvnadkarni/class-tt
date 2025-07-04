367b5f5f5773fe3c8347367b9ac30449c19fefd3
Atharv Nadkarni <atharvmnadkarni@gmail.com>
2025-07-04 17:57:30 +0530

    add password field

--- backend/models/Teacher.js
+++ backend/models/Teacher.js
@@ -9,7 +9,9 @@ const teacherSchema = new mongoose.Schema({
         _id: false,
         subject: String,
         classes: {type: mongoose.SchemaTypes.Mixed}
-    }]
+    }],
+    password: {type: String, required: true}
 })
+
 
 module.exports = mongoose.model("Teacher", teacherSchema);
