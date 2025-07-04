47fb3dd968ab2821d15f4a54dd2fdf85d8bd292f
Atharv Nadkarni <atharvmnadkarni@gmail.com>
2025-07-04 18:39:21 +0530

    add signup static

--- backend/models/Teacher.js
+++ backend/models/Teacher.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const teacherSchema = new mongoose.Schema({
   _id: false,
@@ -13,5 +14,18 @@ const teacherSchema = new mongoose.Schema({
   username: { type: String, unique: true, required: true },
   password: { type: String, required: true },
 });
+
+teacherSchema.statics.signup = async (name, subjects, username, password) => {
+  // check if username exists
+  const exists = await this.findOne({ username });
+  if (exists) {
+    throw Error("Username already exists");
+  }
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+
+  const user = await this.create({ name, subjects, username, password: hash });
+  return user;
+};
 
 module.exports = mongoose.model("Teacher", teacherSchema);
