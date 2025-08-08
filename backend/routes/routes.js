aeb619d60d8b876b0082c8ebbd1c78e531e7cd46
Atharv Nadkarni <atharvmnadkarni@gmail.com>
2025-08-08 13:35:47 +0530

    implement timetable router

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const teacherRouter = require("./teacherRoutes");
 const substitutionRouter = require("./subRoutes");
-// const classRouter = require("./classRoutes");
+const timetableRouter = require("./timetableRoutes");
 
 const router = express.Router();
 router.use('/teacher', teacherRouter)
 router.use('/substitution', substitutionRouter)
-// router.use('/class', classRouter)
+router.use('/timetable', timetableRouter)
 module.exports = router
