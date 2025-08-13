9cf61b298dc108bdc2a57b6460b7f8729c36f167
Atharv Nadkarni <atharvmnadkarni@gmail.com>
2025-08-13 15:20:08 +0530

    add substitution link only if tier is coordinator

--- frontend/tabs.tsx
+++ frontend/tabs.tsx
@@ -1,8 +1,18 @@
 "use client";
 
 import Link from "next/link";
+import { useEffect, useRef } from "react";
+import { Tier } from "./types";
 
 const Tabs = ({ activeTab }) => {
+  const teacherTier = useRef(Tier.TEACHER);
+  useEffect(() => {
+    const { tier } = JSON.parse(
+      localStorage.getItem("user") ?? JSON.stringify({ tier: Tier.TEACHER })
+    );
+    teacherTier.current = tier;
+  }, [localStorage]);
+
   return (
     <div className="max-w-full">
       <div className="mb-6">
@@ -14,8 +24,8 @@ const Tabs = ({ activeTab }) => {
         <div className="flex items-center gap-1 mb-6 bg-gray-100 p-1 rounded-lg w-fit">
           <Link href="/timetable">
             <button
-            onClick={() => {
-                localStorage.setItem("currentTab", "timetable")
+              onClick={() => {
+                localStorage.setItem("currentTab", "timetable");
               }}
               className={`px-4 py-2 text-sm font-medium rounded-md transition-colors ${
                 activeTab === "schedule"
@@ -29,7 +39,7 @@ const Tabs = ({ activeTab }) => {
           <Link href="/teachers">
             <button
               onClick={() => {
-                localStorage.setItem("currentTab", "teachers")
+                localStorage.setItem("currentTab", "teachers");
               }}
               className={`px-4 py-2 text-sm font-medium rounded-md transition-colors ${
                 activeTab === "teachers"
@@ -40,20 +50,22 @@ const Tabs = ({ activeTab }) => {
               Teacher Details
             </button>
           </Link>
-          <Link href="/substitutions">
-            <button
-              onClick={() => {
-                localStorage.setItem("currentTab", "substitutions")
-              }}
-              className={`px-4 py-2 text-sm font-medium rounded-md transition-colors ${
-                activeTab === "subs"
-                  ? "bg-white text-blue-600 shadow-sm"
-                  : "text-gray-600 hover:text-gray-800"
-              }`}
-            >
-              Substitution History
-            </button>
-          </Link>
+          {teacherTier.current == Tier.COORDINATOR && (
+            <Link href="/substitutions">
+              <button
+                onClick={() => {
+                  localStorage.setItem("currentTab", "substitutions");
+                }}
+                className={`px-4 py-2 text-sm font-medium rounded-md transition-colors ${
+                  activeTab === "subs"
+                    ? "bg-white text-blue-600 shadow-sm"
+                    : "text-gray-600 hover:text-gray-800"
+                }`}
+              >
+                Substitution History
+              </button>
+            </Link>
+          )}
         </div>
       </div>
     </div>
