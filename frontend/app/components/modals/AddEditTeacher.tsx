0321cd2f1ad0fe12751ea9c9e0b008c9d191cf94
Atharv Nadkarni <atharvmnadkarni@gmail.com>
2025-08-13 15:06:19 +0530

    change default value of tier to teacher

--- frontend/app/components/modals/AddEditTeacher.tsx
+++ frontend/app/components/modals/AddEditTeacher.tsx
@@ -305,6 +305,7 @@ const AddEditTeacher = ({ mode, setMode, allTeachers, setAllTeachers }) => {
                   <select
                     className="w-full px-3 py-2 mb-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
                     value={tier}
+                    defaultValue={Tier.TEACHER}
                     onChange={(e) => {
                       // setFormData((prev) => ({
                       //   ...formData,
