06911791c203923d028af3d75826f15b42af764e
Atharv Nadkarni <atharvmnadkarni@gmail.com>
2025-08-22 23:50:13 +0530

    add workload icon

--- frontend/app/components/TeacherCard.tsx
+++ frontend/app/components/TeacherCard.tsx
@@ -1,6 +1,6 @@
 import { subjectToDisplayName } from "@/subjects";
 import axios from "axios";
-import { Pencil, Trash2, User } from "lucide-react";
+import { BookCopy, Pencil, Trash2, User } from "lucide-react";
 import { useSelector } from "react-redux";
 import DeleteModal from "./modals/DeleteModal";
 import { useState } from "react";
@@ -26,25 +26,28 @@ const TeacherCard = ({ teacher, setMode, setAllTeachers, tier }) => {
       key={teacher.name}
       className="flex items-center justify-between p-4 border border-gray-200 bg-[#b8defc] rounded-lg hover:bg-[#a6d7ff] transition-colors relative"
     >
-      {(tier == Tier.ADMIN || tier == Tier.COORDINATOR) && <div className="flex absolute top-2 right-2 gap-2">
-        <Pencil
-          className="w-4 h-4"
-          style={{ cursor: "pointer" }}
-          onClick={() =>
-            setMode({
-              mode: "edit",
-              teacher,
-            })
-          }
-        />
-        <Trash2
-          className="w-4 h-4"
-          style={{ cursor: "pointer" }}
-          onClick={() => {
-            setDeleteModalId(teacher._id);
-          }}
-        />
-      </div>}
+      {(tier == Tier.ADMIN || tier == Tier.COORDINATOR) && (
+        <div className="flex absolute top-2 right-2 gap-2">
+          <BookCopy className="w-4 h-4" style={{ cursor: "pointer" }} />
+          <Pencil
+            className="w-4 h-4"
+            style={{ cursor: "pointer" }}
+            onClick={() =>
+              setMode({
+                mode: "edit",
+                teacher,
+              })
+            }
+          />
+          <Trash2
+            className="w-4 h-4"
+            style={{ cursor: "pointer" }}
+            onClick={() => {
+              setDeleteModalId(teacher._id);
+            }}
+          />
+        </div>
+      )}
       <div className="flex items-center gap-4">
         <div className="p-2 bg-primary text-black rounded-lg">
           <User className="h-5 w-5 text-secondary" />
@@ -67,8 +70,8 @@ const TeacherCard = ({ teacher, setMode, setAllTeachers, tier }) => {
                 return [previous, <br key={previous} />, current].flat();
               })}
           </p>
-           <p className="text-sm/[18px] text-gray-600">
-            {teacher.tier}
+          <p className="text-sm/[18px] text-gray-600">
+            {teacher.tier ?? Tier.TEACHER}
           </p>
         </div>
       </div>
