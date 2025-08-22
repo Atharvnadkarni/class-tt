8fc64ddcef2ab8e2a8841e86c3b3377e2620a091
Atharv Nadkarni <atharvmnadkarni@gmail.com>
2025-08-22 18:35:28 +0530

    fix tailwind extend

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -26,14 +26,7 @@ const config: Config = {
           DEFAULT: "hsl(var(--popover))",
           foreground: "hsl(var(--popover-foreground))",
         },
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
-        },
-        secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
-        },
+
         muted: {
           DEFAULT: "hsl(var(--muted))",
           foreground: "hsl(var(--muted-foreground))",
