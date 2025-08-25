8a77d5315bb16ee5aae1c53c21833bc05a46267d
Atharv Nadkarni <atharvmnadkarni@gmail.com>
2025-08-25 13:01:51 +0530

    join classes first before selecting

--- frontend/app/timetable/page.tsx
+++ frontend/app/timetable/page.tsx
@@ -168,9 +168,11 @@ function _TimetablePage() {
                 onChange={(e) => handleClassChange(e.target.value)}
                 className="px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-sm"
               >
-                {classes.map((classe) => (
-                  <option value={classe}>{classe}</option>
-                ))}
+                {classes
+                  .map((classe) => classe.join(""))
+                  .map((classe) => (
+                    <option value={classe}>{classe}</option>
+                  ))}
               </select>
             </div>
           </div>
