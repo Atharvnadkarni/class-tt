da7bf7d5bef1b7a7968a36548380372898665213
Atharv Nadkarni <atharvmnadkarni@gmail.com>
2025-08-25 14:06:53 +0530

    add initial workload modal

--- frontend/app/components/modals/WorkloadModal.tsx
+++ frontend/app/components/modals/WorkloadModal.tsx
@@ -3,24 +3,31 @@ import { X } from "lucide-react";
 import { useEffect, useState } from "react";
 
 const WorkloadModal = ({ setVisibility, teacher }) => {
-  const [workload, setWorkload] = useState({});
+  const [workload, setWorkload] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
       const teacherId = teacher._id;
       const res = await axios.get(
-        "http://localhost:4000/api/teacher/workload/" + teacherId
-      , {headers: {Authorization: `Bearer ${JSON.parse(localStorage.getItem("user")).token}`}});
+        "http://localhost:4000/api/teacher/workload/" + teacherId,
+        {
+          headers: {
+            Authorization: `Bearer ${
+              JSON.parse(localStorage.getItem("user")).token
+            }`,
+          },
+        }
+      );
       const data = await res.data;
-      setWorkload(data);
+      setWorkload(data.workload);
     };
     fetchData();
   }, []);
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4">
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
       <div className="bg-white rounded-lg shadow-xl w-full max-w-md">
         {/* Modal Header */}
-        <div className="flex items-center justify-between p-6 border-gray-200">
+        <div className="flex items-center justify-between p-6 pb-2 border-gray-200">
           <h3 className="text-lg font-semibold text-gray-800">
             {teacher.name}
           </h3>
@@ -30,7 +37,25 @@ const WorkloadModal = ({ setVisibility, teacher }) => {
           >
             <X className="h-5 w-5" />
           </button>
-          {JSON.stringify(workload)}
+        </div>
+        <div className="flex items-center justify-between p-6 pt-2 border-gray-200">
+          <table>
+            <thead className="w-full">
+              <tr className="w-full">
+                <th className="w-24 text-left">Subject</th>
+                <th className="w-24 text-left">Allotted</th>
+              </tr>
+            </thead>
+            <tbody className="w-full">
+              {console.log(workload)}
+              {workload.map((subject) => (
+                <tr className="w-full">
+                  <td className="w-24">{subject.subject}</td>
+                  <td className="w-24">{subject.allotted}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
