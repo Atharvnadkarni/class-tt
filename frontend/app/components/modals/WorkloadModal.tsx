d29f46947640f79a5e8e0f974ac07c93f2efdf76
Atharv Nadkarni <atharvmnadkarni@gmail.com>
2025-08-25 13:47:04 +0530

    add workload system

--- frontend/app/components/modals/WorkloadModal.tsx
+++ frontend/app/components/modals/WorkloadModal.tsx
@@ -1,6 +1,21 @@
+import axios from "axios";
 import { X } from "lucide-react";
+import { useEffect, useState } from "react";
 
 const WorkloadModal = ({ setVisibility, teacher }) => {
+  const [workload, setWorkload] = useState({});
+  useEffect(() => {
+    const fetchData = async () => {
+      const teacherId = teacher._id;
+      const res = await axios.get(
+        "http://localhost:4000/api/teacher/workload/" + teacherId
+      , {headers: {Authorization: `Bearer ${JSON.parse(localStorage.getItem("user")).token}`}});
+      const data = await res.data;
+      setWorkload(data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4">
       <div className="bg-white rounded-lg shadow-xl w-full max-w-md">
@@ -15,6 +30,7 @@ const WorkloadModal = ({ setVisibility, teacher }) => {
           >
             <X className="h-5 w-5" />
           </button>
+          {JSON.stringify(workload)}
         </div>
       </div>
     </div>
