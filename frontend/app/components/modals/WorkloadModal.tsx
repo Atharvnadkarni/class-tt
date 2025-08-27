3082d77531d88f6fd19158eeb81fcc0051c7edce
Atharv Nadkarni <atharvmnadkarni@gmail.com>
2025-08-27 17:18:25 +0530

    add week range

--- frontend/app/components/modals/WorkloadModal.tsx
+++ frontend/app/components/modals/WorkloadModal.tsx
@@ -1,11 +1,27 @@
 import { ReportRange } from "@/types";
 import axios from "axios";
-import { X } from "lucide-react";
+import { formatDate, setWeek } from "date-fns";
+import { ChevronLeft, ChevronRight, X } from "lucide-react";
 import { useEffect, useState } from "react";
+
+const formatDate = (date: Date) => date && date.toISOString().slice(0, 10);
 
 const WorkloadModal = ({ setVisibility, teacher }) => {
   const [workload, setWorkload] = useState([]);
   const [reportRange, setReportRange] = useState(ReportRange.WEEKLY);
+  const [weekRange, setWeekRange] = useState<Date[]>([]);
+
+  useEffect(() => {
+    const today = new Date();
+    const dayOfWeek = today.getDay();
+    const lastMonday = new Date(today);
+    lastMonday.setDate(today.getDate() - ((dayOfWeek + 6) % 7));
+    const lastSaturday = new Date(today);
+    lastSaturday.setDate(lastMonday.getDate() + 5);
+
+    setWeekRange([lastMonday, lastSaturday]);
+  }, []);
+
   useEffect(() => {
     const fetchData = async () => {
       const teacherId = teacher._id;
@@ -63,22 +79,52 @@ const WorkloadModal = ({ setVisibility, teacher }) => {
               Monthly Report
             </button>
           </div>
-            <div>
-            <label className="block text-sm text-gray-600 mb-1">Week Range</label>
+          <div>
+            <label className="block text-sm text-gray-600 mb-1">
+              Week Range
+            </label>
             <div className="flex gap-2">
+              <button
+                onClick={() => {
+                  const [mon, sat] = weekRange;
+                  const nextMonday = new Date(mon);
+                  const nextSaturday = new Date(sat);
+                  nextMonday.setDate(mon.getDate() - 7);
+                  nextSaturday.setDate(sat.getDate() - 7);
+                  setWeekRange([nextMonday, nextSaturday]);
+                }}
+                className="px-2 h-[45px] text-sm font-medium  bg-[lightgrey] text-black hover:bg-[darkgrey] text-black rounded-lg transition-colors flex items-center gap-2"
+              >
+                <ChevronLeft />
+              </button>
               <input
-              type="date"
-              className="px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-              // onChange={...}
+                type="date"
+                value={formatDate(weekRange[0])}
+                className="px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                // onChange={...}
               />
               <span className="self-center text-gray-500">to</span>
               <input
-              type="date"
-              className="px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-              // onChange={...}
+                type="date"
+                value={formatDate(weekRange[1])}
+                className="px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                // onChange={...}
               />
+              <button
+                onClick={() => {
+                  const [mon, sat] = weekRange;
+                  const nextMonday = new Date(mon);
+                  const nextSaturday = new Date(sat);
+                  nextMonday.setDate(mon.getDate() + 7);
+                  nextSaturday.setDate(sat.getDate() + 7);
+                  setWeekRange([nextMonday, nextSaturday]);
+                }}
+                className="px-2 h-[45px] text-sm font-medium  bg-[lightgrey] text-black hover:bg-[darkgrey] text-black rounded-lg transition-colors flex items-center gap-2"
+              >
+                <ChevronRight />
+              </button>
             </div>
-            </div>
+          </div>
         </div>
         <div className="flex items-center justify-between p-6 pt-2 border-gray-200">
           <table>
