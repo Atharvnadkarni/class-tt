420965c4cf5b3eae8ef89ec18fe164632f602e11
Atharv Nadkarni <atharvmnadkarni@gmail.com>
2025-09-24 19:36:09 +0530

    fix app

--- backend/netlify/functions/api.js
+++ backend/netlify/functions/api.js
@@ -17,21 +17,25 @@ const app = express();
 const MONGO_URI = process.env.MONGO_CONNECTION_URI;
 mongoose
   .connect(MONGO_URI)
-  .then(() => console.log("Successfully connected to MongoDB"))
+  .then(() => {
+    console.log("Successfully connected to MongoDB");
+    app.use(express.json());
+    app.use(cors());
+
+    // --- Routes ---
+    // We mount the router at the base path. The /api part is handled by
+    // the redirect rule in netlify.toml.
+    app.use((req, res, next) => {
+      console.log(req, router);
+      next();
+    });
+    app.use("/api", router);
+
+    // --- Export the handler ---
+    // The serverless-http wrapper takes your Express app and converts it
+    // into a handler that Netlify Functions can use.
+  })
   .catch((err) => console.error("Could not connect to MongoDB:", err));
 
-  
 // --- Middleware ---
-app.use(express.json());
-app.use(cors());
-
-// --- Routes ---
-// We mount the router at the base path. The /api part is handled by
-// the redirect rule in netlify.toml.
-app.use((req, res, next) => {console.log(req, router); next()});
-app.use("/api", router);
-
-// --- Export the handler ---
-// The serverless-http wrapper takes your Express app and converts it
-// into a handler that Netlify Functions can use.
 module.exports.handler = serverless(app);
