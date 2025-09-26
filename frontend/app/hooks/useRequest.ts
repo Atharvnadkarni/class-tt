60aba8e9b112ca3ae4add1edd52632a094255e58
Atharv Nadkarni <atharvmnadkarni@gmail.com>
2025-09-26 20:07:44 +0530

    DEBUG

--- frontend/app/hooks/useRequest.ts
+++ frontend/app/hooks/useRequest.ts
@@ -11,8 +11,7 @@ import { useState, useCallback } from "react";
 import { useDispatch } from "react-redux";
 
 export const useRequest = (options: UseRequestOptions = {}) => {
-  const baseURL =
-    options.baseURL || "https://class-tt-backend.onrender.com/api";
+  const baseURL = options.baseURL || "http://localhost:4000/api";
   let token = "";
   if (typeof window !== "undefined") {
     const userStr = window.localStorage.getItem("user");
