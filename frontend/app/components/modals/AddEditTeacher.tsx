352ad34ca1771e2547b1736cfe39d65b062ca50a
Atharv Nadkarni <atharvmnadkarni@gmail.com>
2025-09-26 23:40:09 +0530

    add editableclasses

--- frontend/app/components/modals/AddEditTeacher.tsx
+++ frontend/app/components/modals/AddEditTeacher.tsx
@@ -40,6 +40,7 @@ const AddEditTeacher = ({ mode, setMode, allTeachers, setAllTeachers }) => {
   const [subs, setSubs] = useState(
     mode?.mode == "edit" ? mode?.teacher?.subjects : []
   );
+  const [editableClasses, setEditableClasses] = useState([0, 0]);
   const handleCancel = () => {
     setMode({ mode: null, teacher: null });
     setFormData({ subject: "", div: "", classNo: null, teacherName: null });
@@ -79,6 +80,7 @@ const AddEditTeacher = ({ mode, setMode, allTeachers, setAllTeachers }) => {
       username,
       password,
       tier,
+      editableClasses
     }).catch((err) => console.error(err.message));
     const newTeachers = await request("get", "/teacher");
     setMode(null);
@@ -122,6 +124,7 @@ const AddEditTeacher = ({ mode, setMode, allTeachers, setAllTeachers }) => {
       displayName,
       ...(password != "-------" && { password }),
       tier,
+      editableClasses
     });
     const newTeachers = await request("get", "/teacher");
     setMode(null);
@@ -137,12 +140,14 @@ const AddEditTeacher = ({ mode, setMode, allTeachers, setAllTeachers }) => {
       setDisplayName(mode.teacher.displayName);
       setUsername(mode.teacher.username);
       setPassword("-------");
+      setEditableClasses(mode.teacher.editableClasses)
       console.log(mode.teacher.subjects);
     } else {
       setTeacherName("");
       setDisplayName("");
       setUsername("");
       setPassword("");
+      setEditableClasses([0,0])
     }
     const subs = mode?.teacher?.subjects;
     if (subs) {
@@ -301,7 +306,7 @@ const AddEditTeacher = ({ mode, setMode, allTeachers, setAllTeachers }) => {
             <div className="tiers">
               <div className="subject">
                 <label
-                  htmlFor="class-taught"
+                  htmlFor="level"
                   className="block text-sm font-medium text-gray-700 mb-2"
                 >
                   Level
@@ -329,6 +334,83 @@ const AddEditTeacher = ({ mode, setMode, allTeachers, setAllTeachers }) => {
                     <option value={Tier.COORDINATOR}>Coordinator</option>
                     <option value={Tier.TEACHER}>Teacher</option>
                   </select>
+                  {/* {} */}
+
+                  {/* <select
+                    className="w px-3 py-2 mb-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                    value={formData.div}
+                    onChange={(e) => {
+                      setFormData((prev) => ({
+                        ...prev,
+                        div: e.target.value,
+                      }));
+                      // setFormData((prev) => ({
+                      //   ...prev,
+                      //   subjects: [
+                      //     {
+                      //       ...prev.subjects[i],
+                      //       div: e.target.value,
+                      //     },
+                      //     ...prev.subjects.filter(
+                      //       (elem, ind) => ind != i
+                      //     ),
+                      //   ],
+                      // }));
+                    }}
+                  >
+                    <option value="">Division</option>
+                    <option value="A">A</option>
+                    <option value="B">B</option>
+                    <option value="C">C</option>
+                    <option value="AB">AB</option>
+                    <option value="AC">AC</option>
+                    <option value="BC">BC</option>
+                    <option value="ABC">ABC</option>
+                  </select> */}
+                </div>
+              </div>
+              {/* Subject Input */}
+            </div>
+            <div className="classes-taught">
+              <div className="subject">
+                <label
+                  htmlFor="class-taught"
+                  className="block text-sm font-medium text-gray-700 mb-2"
+                >
+                  Classes Taught
+                </label>
+                <div className="flex w-full">
+                  <input
+                    type="number"
+                    id="teacherName"
+                    value={editableClasses[0]}
+                    onChange={(e) => {
+                      setEditableClasses((oec) => [
+                        e.target.value,
+                        ...oec.slice(1),
+                      ]);
+                    }}
+                    placeholder="Class"
+                    className="w-[80px] px-3 py-2 mb-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                  />
+                  <span
+                    className={`w-[calc(100vw-160px)] px-3 py-2 mb-2 text-center`}
+                  >
+                    to
+                  </span>
+                  <input
+                    type="number"
+                    id="teacherName"
+                    value={editableClasses[1]}
+                    onChange={(e) => {
+                      setEditableClasses((oec) => [
+                        ...oec.slice(0, -1),
+                        e.target.value,
+                      ]);
+                    }}
+                    placeholder="Class"
+                    className="w-[80px] px-3 py-2 mb-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                  />
                   {/* {} */}
 
                   {/* <select
