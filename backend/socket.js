04f50ffbe225c8a5ed801065a0b66c9f9c0a9cd3
Atharv Nadkarni <atharvmnadkarni@gmail.com>
2025-09-30 20:22:36 +0530

    make socketserver

--- backend/socket.js
+++ backend/socket.js
@@ -1,11 +1,21 @@
 const { Server } = require("socket.io");
 const http = require("http");
+const redisClient = require("./redis");
 
 const socketListener = http.createServer(4000);
 const socketServer = new Server(socketListener, {
   cors: { origin: "*" },
-})
+});
+socketServer.on("connection", (client) => {
+  console.log("User connected", client.id);
+  client.on("save_attendance", async (record) => {
+    await redisClient.publish("save_attendance", record)
+  });
+  client.on("disconnect", () => {
+    console.log("User disconnected:", client.id);
+  });
+});
 module.exports = {
-    http: socketListener,
-    io: socketServer
-}
+  http: socketListener,
+  io: socketServer,
+};
