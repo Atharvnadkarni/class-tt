9d2385021bd8c74c14d4fb8e0e4b126246f60268
Atharv Nadkarni <atharvmnadkarni@gmail.com>
2025-10-04 14:28:18 +0530

    add basic attendancemodal

---
+++ frontend/app/components/modals/AttendanceModal.tsx
@@ -0,0 +1,36 @@
+import { subjectToDisplayName } from "@/subjects";
+import { ReportRange } from "@/types";
+import { useRequest } from "@/app/hooks/useRequest";
+import { formatDate, setWeek } from "date-fns";
+import { ChevronLeft, ChevronRight, X } from "lucide-react";
+import { useEffect, useState } from "react";
+
+const formatDate = (date: Date) => date && date.toISOString().slice(0, 10);
+
+interface WorkloadItem {
+  subject: string;
+  class?: string;
+  allotted?: number;
+  taken?: number;
+}
+
+const AttendanceModal = ({
+  setVisibility,
+  attendanceRecord,
+}: {
+  setVisibility: (v: boolean) => void;
+  attendanceRecord: any;
+}) => {
+  
+  
+
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+      <div className="bg-white rounded-lg shadow-xl w-3/4 h-3/4">
+        {/* Modal Header */}
+        {attendanceRecord}
+      </div>
+    </div>
+  );
+};
+export default AttendanceModal;
