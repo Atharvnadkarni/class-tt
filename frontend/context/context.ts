d6fed09897cedff3fba86494ae3160833bd9cd40
Atharv Nadkarni <atharvmnadkarni@gmail.com>
2025-10-22 16:04:51 +0530

    reset items

--- frontend/context/context.ts
+++ frontend/context/context.ts
@@ -1,10 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
-import attendanceReducer from "./attendanceSlice";
 
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-    attendance: attendanceReducer,
-  },
+  reducer: userReducer,
 });
