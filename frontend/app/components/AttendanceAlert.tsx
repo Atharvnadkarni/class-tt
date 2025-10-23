3dba6cac9e1fbb1eb94577e4a56580f639669cfe
Atharv Nadkarni <atharvmnadkarni@gmail.com>
2025-10-23 19:02:19 +0530

    invoke optional chain

--- frontend/app/components/AttendanceAlert.tsx
+++ frontend/app/components/AttendanceAlert.tsx
@@ -7,7 +7,7 @@ const AttendanceAlert = () => {
   return (
     <div
       className="h-7 w-[100vw] bg-red-700 text-white  flex items-center justify-center -mt-6 cursor-pointer"
-      onClick={() => setModalVisible(true)}
+      onClick={() => setModalVisible?.(true)}
     >
       <TriangleAlert className="mr-2" /> Absent teachers' periods have not been
       fully substituted.
