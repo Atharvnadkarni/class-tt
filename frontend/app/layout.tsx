5c8a5307d094d0c5ac304e5fe9e0414e5a4b048b
Atharv Nadkarni <atharvmnadkarni@gmail.com>
2025-10-23 16:34:29 +0530

    delete attendancealert from layout

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -17,7 +17,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <AttendanceAlert />
         <Providers>{children}</Providers>
       </body>
     </html>
