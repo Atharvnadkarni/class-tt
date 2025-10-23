2b4a1c9049f87d98b538b7497f6fe3704c6e6264
Atharv Nadkarni <atharvmnadkarni@gmail.com>
2025-10-23 18:28:47 +0530

    conditionally render attendancealert

--- frontend/app/components/Dashboard.tsx
+++ frontend/app/components/Dashboard.tsx
@@ -1,11 +1,20 @@
 import Tabs from "@/tabs";
 import AttendanceAlert from "./AttendanceAlert";
+import { useAppSelector } from "@/context/contextHooks";
+
+interface AttendanceRecord {
+  [key: string]: boolean;
+}
 
 const Dashboard = (props) => {
+  const attendanceRecord: AttendanceRecord = useAppSelector(
+    (state) => state.attendance.record
+  );
+  const allPresent = Object.values(attendanceRecord).every((a) => a);
   return (
     <div className="w-full -mx-4">
       <Tabs activeTab={props.activeTab} />
-      <AttendanceAlert />
+      {allPresent && <AttendanceAlert />}
     </div>
   );
 };
