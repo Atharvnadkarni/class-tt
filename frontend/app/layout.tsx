b291f3b71d82389ba85bf7d0cd2f804d455114a7
Atharv Nadkarni <atharvmnadkarni@gmail.com>
2025-10-28 15:18:01 +0530

    change prop

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={null} />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:ital,wght@0,300..900;1,300..900&display=swap"
           rel="stylesheet"
